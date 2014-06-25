58e08673c045fa2e8f11f5f438e38806bc1ef585
Colin Pierce <cpierce46@gmail.com>
2014-06-25 12:47:42 -0600

    initial check in

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Change_Maker2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this will be fun
+            Console.ReadKey();
+        }
+        static void ChangeMaker(float amount)
+        {
+
+        }
+    }
+}
